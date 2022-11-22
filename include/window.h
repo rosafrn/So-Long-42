bbbcb629de9a48677bbb66f900de894748f69ace
Rosa Fernandes <rosferna@c2r8s6.42lisboa.com>
2022-11-22 18:58:52 +0000

    added new_program

--- include/window.h
+++ include/window.h
@@ -1,12 +1,26 @@
 #ifndef WINDOW_H
 # define WINDOW_H
 
-typedef struct s_win
+typedef struct s_winn
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
 	int		height;
 	int		width;
-}		t_win;
+}		t_winn;
+
+typedef struct s_imgg
+{
+	t_winn	win;
+	void	*img_ptr;
+	char	*addr;
+	int		h;
+	int		w;
+	int		bpp;
+	int		endian;
+	int		line_len;
+}		t_imgg;
+
+void *new_program(int w, int h, char *str);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -4,25 +4,26 @@
 #include "../include/window.h"
 #include <stdio.h>
 
-int main ()
+int main()
 {
-    void    *mlx_ptr;
+    void    *win_ptr;
+
+    win_ptr = NULL;
+    win_ptr = new_program(300, 300, "so_long");
+    return (0);
+}
+
+void *new_program(int w, int h, char *str)
+{
+	void    *mlx_ptr;
     void    *win_ptr;
 
     mlx_ptr = mlx_init();
     if (!mlx_ptr)
-        return (1);
-    win_ptr = mlx_new_window(mlx_ptr, 300, 300, "so_long");
+        return (NULL);
+    win_ptr = mlx_new_window(mlx_ptr, w, h, str);
     if (!win_ptr)
-        return (2);
+        return (NULL);
     mlx_loop(mlx_ptr);
-    return (0);
+    return (win_ptr);
 }
-
-/*t_win new_program(int w, int h, char *str)
-{
-	void	*mlx_ptr;
-
-	mlx_ptr = mlx_init();
-	return ((t_win) {mlx_ptr, mlx_new_window(mlx_ptr, w, h, str), w, h});
-}*/
